249bf62fd10d4279f27ce236f17f77140d745282
wilailuxCsubu <wilailux.ha.57@ubu.ac.th>
2017-09-01 21:12:13 +0700

    first

---
+++ nodejs-01/myfirst/index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('wilailux wanngerng 5711403407');
+}).listen(8080); 
